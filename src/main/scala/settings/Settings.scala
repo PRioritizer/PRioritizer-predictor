2b7a43b095f2559b495add66842c61aa589f73a9
Erik van der Veen <erikvdv1@gmail.com>
2014-07-24 13:12:42 +0200

    Add window and ghtorrent settings

---
+++ src/main/scala/settings/Settings.scala
@@ -0,0 +1,70 @@
+package settings
+
+import java.io.{BufferedReader, FileNotFoundException, InputStreamReader}
+import java.util.Properties
+import scala.collection.JavaConverters._
+
+object PredictorSettings {
+  lazy val modelDirectory = Settings.get("model.directory").getOrElse("")
+  lazy val repositoryOwner = Settings.get("repository.owner").getOrElse("")
+  lazy val repositoryName = Settings.get("repository.name").getOrElse("")
+  lazy val windowInterval = Settings.get("repository.name").getOrElse("")
+  lazy val windowLimit = Settings.get("repository.name").getOrElse("")
+}
+
+object GHTorrentSettings {
+  lazy val host = Settings.get("ghtorrent.host").getOrElse("localhost")
+  lazy val port = Settings.get("ghtorrent.port").fold(3306)(p => p.toInt)
+  lazy val username = Settings.get("ghtorrent.username").getOrElse("")
+  lazy val password = Settings.get("ghtorrent.password").getOrElse("")
+  lazy val database = Settings.get("ghtorrent.database").getOrElse("")
+}
+
+object Settings {
+  val fileName = "settings.properties"
+  val resource = getClass.getResourceAsStream("/" + fileName)
+  val data = read
+
+  /**
+   * @param property The name of the property.
+   * @return True iff there exists a property with the give name.
+   */
+  def has(property: String): Boolean =
+    data.get(property).isDefined
+
+  /**
+   * @param property The name of the property.
+   * @return The value of the property.
+   */
+  def get(property: String): Option[String] =
+    data.get(property)
+
+  /**
+   * Read the properties from the config file.
+   * @return A map with the properties.
+   */
+  private def read: Map[String, String] = {
+    if (resource == null)
+      throw new FileNotFoundException(
+        s"The configuration file was not found. Please make sure you copied $fileName.dist to $fileName.")
+
+    // Read properties file
+    val reader = new BufferedReader(new InputStreamReader(resource, java.nio.charset.StandardCharsets.UTF_8))
+    val props = new Properties
+    props.load(reader)
+    props.readSystemOverride()
+    props.asScala.toMap
+  }
+
+  implicit class RichProperties(properties: Properties) {
+    def readSystemOverride(): Unit = {
+      val keys = properties.keySet().asScala.collect({ case str: String => str })
+
+      keys.foreach(key => {
+        val propOverride = System.getProperty(key)
+        if (propOverride != null)
+          properties.put(key, propOverride)
+      })
+    }
+  }
+}
