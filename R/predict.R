c8e3a292d8f9961e12b4aadb6675e12a0c1d21f6
Erik van der Veen <erikvdv1@gmail.com>
2014-08-06 11:55:52 +0200

    Predict value with saved model

---
+++ R/predict.R
@@ -0,0 +1,38 @@
+### Install packages
+source("helper/install-packages.R")
+
+### Include helpers
+source("algorithms/random-forest.R")
+source("helper/data.R")
+source("helper/model.R")
+
+# ================================== PROGRAM ================================== #
+
+### Files
+dir <- "~/git/prioritizer/predictor"
+model.file <- paste(dir, "model.RData", sep = "/")
+
+### Input
+input <- data.frame(age              = 0,
+                    coreMember       = 1,
+                    commitRatio      = 0.33,
+                    pullRequestRatio = 0.75,
+                    comments         = 0,
+                    reviewComments   = 0,
+                    additions        = 10,
+                    deletions        = 5,
+                    commits          = 1,
+                    files            = 1,
+                    important        = 0,
+                    stringsAsFactors = FALSE)
+
+# input <- read.data(paste(dir, "zbmc-100.csv", sep = "/"))
+data2 <- prepare.data(input)
+
+### Read trained model (access via the trained.model variable)
+load(model.file)
+
+### Predict value
+predictions <- random.forest.predict(trained.model, data2)
+predictions <- as.logical(predictions)
+print(predictions)
