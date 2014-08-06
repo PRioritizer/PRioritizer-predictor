c60eabd6c7f19468d12a527ad6be1a7ca5002845
Erik van der Veen <erikvdv1@gmail.com>
2014-08-06 11:55:10 +0200

    Add test file

---
+++ R/test/test.R
@@ -0,0 +1,35 @@
+### Install packages
+source("helper/install-packages.R")
+
+### Include helpers
+source("algorithms/all.R")
+source("helper/data.R")
+source("helper/evaluation.R")
+source("helper/model.R")
+source("helper/utils.R")
+
+# ================================== PROGRAM ================================== #
+
+### Files
+dir <- "~/git/prioritizer/predictor"
+input.file <- paste(dir, "xbmc-100.csv", sep = "/")
+output.file <- paste(dir, "model.RData", sep = "/")
+
+### Read data
+data <- read.data(input.file)
+data <- split.data(data, .75)
+
+### Train model
+trained.model <- random.forest.train(model, data$train)
+save(trained.model, file = output.file)
+
+### Read trained model (access via the trained.model variable)
+rm(trained.model)
+load(model.file)
+
+### Predict value
+test <- head(data$test, n = 1)
+predictions <- random.forest.predict(trained.model, test)
+predictions <- as.logical(predictions)
+print(predictions)
+
