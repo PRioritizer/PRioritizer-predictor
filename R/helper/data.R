125886829a2e2892330a21a345d02e816331d87e
Erik van der Veen <erikvdv1@gmail.com>
2014-08-06 10:40:51 +0200

    Fix data frame shuffle

--- R/helper/data.R
+++ R/helper/data.R
@@ -5,13 +5,14 @@
 # l[2] testing dataset
 split.data <- function(data, split = .5) {
   # Shuffle data
+  n <- nrow(data)
   newData <- data[sample.int(nrow(data)),]
 
   # TODO: split such that all targets are present in first set
 
   # Split data into training and test data
-  newData.train <- data[1:floor(nrow(data)*split), ]
-  newData.test <- data[(floor(nrow(data)*split)+1):nrow(data), ]
+  newData.train <- newData[1:floor(n*split), ]
+  newData.test <- newData[(floor(n*split)+1):n, ]
   list(train = newData.train, test = newData.test)
 }
 
